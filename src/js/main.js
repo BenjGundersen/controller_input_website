782ca2e0a0c66a0b46fcba915f2baffc108bf407
BenjGundersen <benjgunder@gmail.com>
2025-02-15 16:46:37 -0700

    made it so there is feedback for button presses

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 // Check for when a gamepad is connected
-
 window.addEventListener("gamepadconnected", (event) => {
     const gamepad = event.gamepad;
     document.querySelector(".status").textContent = `Gamepad connected: ${gamepad.id}`;
     console.log(gamepad.id);
     checkControllerType(gamepad);
     document.querySelector(".controller-outline").style.display = "block";
-    updateLoop();
+    document.querySelector(".buttons-section").style.display = "block";
+    updateGamepad();
 });
 
 
@@ -14,14 +14,18 @@ window.addEventListener("gamepadconnected", (event) => {
 window.addEventListener("gamepaddisconnected", (event) => {
     document.querySelector(".status").textContent = "No gamepad connected.";
     document.querySelector(".controller-outline").style.display = "None";
+    document.querySelector(".buttons-section").style.display = "None";
+    document.querySelector(".buttons-section").innerHTML = "";
 
 });
+
 
 // Check for what type of controller is connected and display an appropriate svg file, removes the svg file from the site as the gamepad is disconnected.
 function checkControllerType(gamepad) {
     let controllerType = document.querySelector(".controller-outline");
     if (gamepad.id.includes("DualSense")) {
         controllerType.setAttribute("src", "/controller_input_website/src/img/dualsense-svgrepo-com.svg")
+        updateButtionsSection(gamepad);
     } else {
         controllerType.style.display = "none";
     }
@@ -33,18 +37,33 @@ function checkControllerType(gamepad) {
     }
 }
 
+// Update the buttons-section div with all the buttons from the gamepad
+function updateButtionsSection(gamepad) {
+    for (let i = 0; i < gamepad.buttons.length; i++) {
+        document.querySelector(".buttons-section").insertAdjacentHTML("beforeend",
+            `<div>
+                Button ${i+1}
+                <div id="box${i+1}" class="feedback-box"></div>
+            </div>`);
+    }
+};
+
+
 // Constantly updates when input from a controller is received
-function updateLoop() {
-    const gamepads = navigator.getGamepads();
-    for (const gamepad of gamepads) {
-        if (gamepad) {
-            for (let i = 0; i < gamepad.buttons.length; i++) {
-                console.log(`Button ${i}: ${gamepad.buttons[i].pressed ? "Pressed" : "Released"}`);
-            }
-            for (let i = 0; i < gamepad.axes.length; i++) {
-                console.log(`Axis ${i}: ${gamepad.axes[i]}`);
-            }
+function updateGamepad() {
+    let gamepads = navigator.getGamepads() ? navigator.getGamepads() : [];
+    if (!gamepads) return;
+
+    let gp = gamepads[0];
+    if (!gp) return;
+
+    for (let i = 0; i < gp.buttons.length; i++) {
+        let box = document.getElementById("box" + (i + 1));
+        if (gp.buttons[i].pressed) {
+            box.classList.add("filled");
+        } else {
+            box.classList.remove("filled")
         }
     }
-    requestAnimationFrame(updateLoop);
+    requestAnimationFrame(updateGamepad);
 }
